4f7a5ad4853ffd8adc12c97a0bd86eb822bcbacf
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-03-23 11:33:16 -0300

    Mudei o banco de dados para postgre e a lib pyodbc para psycopg2, removi a função connector e modifiquei as funções que interagiam com ela.

--- db.py
+++ db.py
@@ -1,36 +1,31 @@
-import pyodbc
+import psycopg2
 
 
-def connector():
-    data_conn = (
-        "Driver={SQL Server};"
-        "Server=DESKTOP-G90E8IG\SQLSERVER;"
-        "Database=all_data;"
-        "Trusted_Connection=Yes"
-    )
+conn = psycopg2.connect(
+    host="localhost",
+    database="all_data",
+    user="postgres",
+    password="postgres")
 
-    conn = pyodbc.connect(data_conn)
+cursor = conn.cursor()
 
-    cursor = conn.cursor()
-    return cursor
+conn.commit()
 
 
 def mod_verifier(nick):
-    cursor = connector()
     string_sql = f"""
         SELECT * FROM usuarios 
         where nick = '{nick}' ;
     """
+
     cursor.execute(string_sql)
     row = cursor.fetchone()
     return row
 
 
 def insert(a, b, c):
-    cursor = connector()
-
     add_user = f"""INSERT INTO usuarios(nome, nick, senha)
         VALUES ('{a}', '{b}', '{c}')"""
 
     cursor.execute(add_user)
-    cursor.commit()
+    conn.commit()
